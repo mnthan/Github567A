733875640bb53bb9ca09e31dca526b599234402a
mnthan <72927773+mnthan@users.noreply.github.com>
2021-03-09 22:05:28 -0500

    Update HW4a.py

--- HW4a.py
+++ HW4a.py
@@ -7,16 +7,18 @@ Created on Tue Mar  9 19:23:11 2021
 """
 
 import json,requests,pprint
-name = input("Enter the username: ")
-url = "https://api.github.com/users/{}/repos".format(name)
 
-r = requests.get(url)
-z = json.loads(r.text)
-#pprint.pprint(z)
-for i in z:
-    
-    url2="https://api.github.com/repos/{}/{}/commits".format(name,i["name"])
-    r2 =requests.get(url2)
-    z2 = json.loads(r2.text)
-    #pprint.pprint(len(z2))
-    print("Repo: "+i["name"]+" Number of commits: "+str(len(z2)))
+def RepoLister(name):
+    #name = input("Enter the username: ")
+    url = "https://api.github.com/users/{}/repos".format(name)
+
+    r = requests.get(url)
+    z = json.loads(r.text)
+    #pprint.pprint(z)
+    for i in z:
+        
+        url2="https://api.github.com/repos/{}/{}/commits".format(name,i["name"])
+        r2 =requests.get(url2)
+        z2 = json.loads(r2.text)
+        #pprint.pprint(len(z2))
+        print("Repo: "+i["name"]+" Number of commits: "+str(len(z2)))
